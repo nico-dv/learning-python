29e1421e383eef22bcc74f2c83ce982bf77905fb
nico-dv <vv.nicov@gmail.com>
2022-07-11 23:16:23 +0100

    added graph creation

---
+++ habit tracker/main.py
@@ -0,0 +1,29 @@
+from lib2to3.pgen2 import token
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": os.getenv("token"),
+    "username": os.getenv("username"),
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{user_params['token']}/graphs"
+
+graph_config = {
+    "id":"graph1",
+    "name":"Coding Graph",
+    "unit": "Mins",
+    "type":"int",
+    "color": "ajisai",
+}
+requests.post(url=graph_endpoint, json=graph_config)
